c4185c561f4d05de673f8a132878a2a3c4fb9ccf
Ryan Thomas <rrthomas1913@gmail.com>
2022-12-16 23:28:57 -0500

    wave 1 complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+
 
 const App = () => {
+  // lifting state..single source -useState
+  // brains
+  console.log(chatMessages);
+  // const [chatData, setChatData] = useState(chatMessages)
+
+
+  // beauty
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/}
+        {/* lines 24-26 in Apps become the props */}
+      <ChatEntry 
+        sender ={chatMessages[0].sender}
+        body ={chatMessages[0].body}
+        timestamp ={chatMessages[0].timeStamp} 
+      ></ChatEntry>    
+        
+        
+        {/*Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  // taking these props from App
+  
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">
+          <TimeStamp time={props.timeStamp}></TimeStamp>
+          </p>
         <button className="like">🤍</button>
       </section>
     </div>
