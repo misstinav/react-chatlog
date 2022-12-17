b60447865653873b2128d239cf93b506f54a81fd
Ryan Thomas <rrthomas1913@gmail.com>
2022-12-17 01:03:37 -0500

    Wave 2 complete, all test passing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
+// import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 
 
 const App = () => {
@@ -18,16 +19,18 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component*/}
-        {/* lines 24-26 in Apps become the props */}
+        {/* Wave 01: Render one ChatEntry component
+        lines 24-26 in Apps become the props
       <ChatEntry 
         sender ={chatMessages[0].sender}
         body ={chatMessages[0].body}
         timestamp ={chatMessages[0].timeStamp} 
       ></ChatEntry>    
-        
+         */}
         
         {/*Wave 02: Render ChatLog component */}
+        {/* components are the tags and props are the attributes */}
+        <ChatLog entries={chatMessages}></ChatLog>
       </main>
     </div>
   );
